26d834395ddf36c445cc039f8b6a7d126b762b21
Quentin <quentin01.bource@gmail.com>
2022-12-09 13:53:52 +0100

    Ex3

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-/*
+/* Ex 1
 */
+document.getElementById("firstname").onkeyup = function() {
+let firstname = document.getElementById("firstname");
+let display_name = document.getElementById("display-firstname");
+display_name.innerText = firstname.value };
+
+
+/* Ex 2 */
+document.getElementById("age").onkeyup = function() {
+    let age = document.getElementById("age")
+    let truth = document.getElementById("a-hard-truth")
+
+        if (age.value >= 18)
+            truth.style.visibility = "visible";
+        else 
+        truth.style.visibility = "hidden";
+};
+
